aa6a2eaac0aab9eedcb862a892ffbbf6967feb82
Vasya Pupkin <vasya@localhost>
2019-05-19 23:17:19 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,27 @@
+//Задача 1
+
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let res = (algebra + geography + physics) / 3;
+console.log(res);
+
+//Задача 2
+
+let name = "Andrey";
+let message = `Hello, world! My name ${name}`;
+console.log(message);
+
+//Задача 3
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
